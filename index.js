dc5a9683fb74408ab1092d5b2a5fbb581fffb473
Angie <amaticorena103@hotmail.com>
2021-11-02 18:39:33 -0400

    commit tree 125abfcd568e249d64255194afbac1d3dc0c9f2e

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1=2
+const num2=31
 
-// Write your code here
+let multiply = num1*num2
+
+let random = Math.floor(Math.random() *10) + 1
+
+const num3 = 40
+const num4 = 6
+
+let mod = num3 % num4
+
+let max = Math.max(1,10,20)
